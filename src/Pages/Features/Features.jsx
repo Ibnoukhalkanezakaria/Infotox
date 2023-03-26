f5dacb11a8aec2064497dd3baafe2f7efd2ebcb5
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-03-26 23:28:22 +0000

    new

--- src/Pages/Features/Features.jsx
+++ src/Pages/Features/Features.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import IntegrateSection from "../../Components/Sections/Home/IntegrateSection";
+import RunsSection from "../../Components/Sections/Home/RunsSection";
+import CtaSection from "../../Components/Sections/Home/CtaSection";
 const Features = () => {
-  return <div></div>;
+  return (
+    <>
+      <IntegrateSection />
+      <RunsSection />
+      <CtaSection />
+    </>
+  );
 };
 
 export default Features;
