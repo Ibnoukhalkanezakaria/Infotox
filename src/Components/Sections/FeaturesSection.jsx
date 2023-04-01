4e274837cfbfaa8575b51a2edb805655ce465372
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-04-01 22:58:08 +0000

    new

--- src/Components/Sections/FeaturesSection.jsx
+++ src/Components/Sections/FeaturesSection.jsx
@@ -10,46 +10,92 @@ const FeaturesSection = () => {
         <h1 className="fw-500 text-5xl items-center secondary-clr leading-3">
           Features
         </h1>
-        <div className="flex md:flex-row flex-wrap gap-6 py-6 text-center justify-center">
-          <div>
-            <span className="fw-600 text-xl secondary-clr ">Plan Features</span>
+        <div className="flex gap-8 flex-wrap">
+          <div className="bg-orange-500 p-2 basis-[700px] ">
+            <span>1</span>
           </div>
-          <div>
-            <span className="fw-600 text-xl secondary-clr ">Professional</span>
-          </div>
-          <div>
-            <span className="fw-600 text-xl secondary-clr ">Premium</span>
-          </div>
-          <div>
-            <span className="fw-600 text-xl secondary-clr ">Entreprise</span>
+          <div className="flex gap-8 justify-between basis-[500px]">
+            <div className="bg-orange-500 p-2 basis-[200px]">
+              <span>2</span>
+            </div>
+            <div className="bg-orange-500 p-2 basis-[200px]">
+              <span>3</span>
+            </div>
+            <div className="bg-orange-500 p-2 basis-[200px]">
+              <span>4</span>
+            </div>
           </div>
         </div>
-        <hr />
-        <div className="pt-10">
+      </div>
+    </div>
+  );
+};
+
+export default FeaturesSection;
+//  <div className="flex md:flex-row md:text-left  text-center md:flex-nowrap flex-wrap gap-8  justify-center pb-6 pt-4">
+//           <div className="md:basis-1/2 basis-1/1 bg-indigo-600">
+//             <span className="fw-600 text-xl secondary-clr ">Plan Features</span>
+//           </div>
+//           <div className="md:basis-1/4 bg-indigo-600">
+//             <span className="fw-600 text-xl secondary-clr ">Professional</span>
+//           </div>
+//           <div className="md:basis-1/4 bg-indigo-600">
+//             <span className="fw-600 text-xl secondary-clr ">Premium</span>
+//           </div>
+//           <div className="md:basis-1/4 bg-indigo-600">
+//             <span className="fw-600 text-xl secondary-clr ">Entreprise</span>
+//           </div>
+//         </div>
+//         <hr />
+
+//         <div className="pt-10">
+//           <div className="flex text-center md:flex-row flex-col justify-center md:flex-nowrap flex-wrap gap-8">
+//             <div>
+//               <div className="md:basis-1/2 bg-orange-600">
+//                 <span>Projects</span>
+//               </div>
+//             </div>
+//             <div className="md:basis-1/4 basis-1/3 bg-orange-600">
+//               <span>1</span>
+//             </div>
+//             <div className="md:basis-1/4 basis-1/3 bg-orange-600">
+//               <span>2</span>
+//             </div>
+//             <div className="md:basis-1/4 basis-1/3 bg-orange-600">
+//               <span>3</span>
+//             </div>
+//           </div>
+//         </div>
+
+{
+  /* <div className="pt-10">
           {PlanFeatures.map((item, i) => {
             const { Professional, Premium, Entreprise, name } = item;
             return (
-              <div key={i} className="flex flex-row gap-6">
+              <div
+                key={i}
+                className="flex md:flex-row md:no-wrap flex-col tex-center items-center justify-center gap-6"
+              >
                 <div className="basis-1/2">
                   <span className="pt-4 pb-10 block fw-400 text-xl secondary-clr">
                     {name}
                   </span>
                 </div>
-                <div className="basis-1/4 pl-[70px]">
+                <div className="md:basis-1/4 basis-1/2 bg-indigo-500 w-[20px]">
                   {Professional ? (
                     <AiOutlineCheck className="bg-[#FEF4E2] color4 rounded-full p-1 text-3xl" />
                   ) : (
                     ""
                   )}
                 </div>
-                <div className="basis-1/4 pl-[70px]">
+                <div className="md:basis-1/4 basis-1/2 bg-indigo-500 w-[20px]">
                   {Premium ? (
                     <AiOutlineCheck className="bg-[#FEF4E2] color4 rounded-full p-1 text-3xl" />
                   ) : (
                     ""
                   )}
                 </div>
-                <div className="basis-1/4 pl-[70px]">
+                <div className="md:basis-1/4 basis-1/2 bg-indigo-500 w-[20px]">
                   {Entreprise ? (
                     <AiOutlineCheck className="bg-[#FEF4E2] color4 rounded-full p-1 text-3xl" />
                   ) : (
@@ -59,10 +105,5 @@ const FeaturesSection = () => {
               </div>
             );
           })}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default FeaturesSection;
+        </div> */
+}
